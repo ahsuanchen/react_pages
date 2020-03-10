044eb890711227ca63d111089f0b7024473a64d8
Jennyangs <hollister0330827@gmail.com>
2020-03-10 18:42:56 +0800

    0310

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
-
 //import App from './App';
 import ReactApp from './components/react-app.jsx';
 import signIn from './components/signs/signIn.jsx';
