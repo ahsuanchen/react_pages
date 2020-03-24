e9f00a94d07dd7c7ea5d2129ddfbcf8c8b3fa600
ahsuanchen <ahsuan1027@gmail.com>
2020-03-24 17:19:22 +0800

    537

--- src/components/profile/trainingFace.jsx
+++ src/components/profile/trainingFace.jsx
@@ -1,228 +1,175 @@
-import React , { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import Header from '../header/PF_header1.jsx';
-import { Link } from 'react-router-dom';
-import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
+import profile from 'assets/images/panda.jpg';
+import Container from '@material-ui/core/Container';
+import SaveIcon from '@material-ui/icons/Save';
 import SyncAltIcon from '@material-ui/icons/SyncAlt';
-import BackupIcon from '@material-ui/icons/Backup';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
-    div : {
-        boxSizing : "border-box"
+const style = {
+    toolbar : {
+        width : '97%' ,
+        height : '50px' ,
+        background : 'linear-gradient(50deg, #00bfa5 40%, #00acc1 85%)' ,
+        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
     } ,
-    left_menu : {
-        display: "flex" ,
-        justifyContent : "space-around" ,
-        minHeight : 800 ,
-        color : "#000"
+    back_button : {
+        marginRight : '5px' ,
     } ,
-    left_container : {
-        maxWidth : "280px" , 
-        borderRight : "1px solid" ,
+    title : {
+        marginLeft : '20px' ,
     } ,
-    avatar : {
-        minWidth : "150px" , 
-        minHeight : "150px" ,
+    line1 : {
+        position : 'absolute' ,
+        left : '4%' ,
+        top : '23%'
     } ,
-    link : {
-        textDecoration : "none" , 
-        color : "#D0D0D0" ,
-        '&:hover' : {
-          color : '#00AEAE' 
-        }
+    line2 : {
+        position : 'absolute' ,
+        left : '4%' ,
+        top : '60%'
     } ,
-    content : {
-        margin : "2% 2%" ,
+    line3 : {
+        position : 'absolute' ,
+        left : '16%' ,
+    } ,
+    text_leftside : {
+        position : 'absolute',
+        top : '15%' ,
+        left : '3%' ,
+        color : '#D0D0D0'
+    } ,
+    text_title : {
+        position : 'absolute',
+        top : '14%' ,
+        left : '20%' ,
     } ,
     form : {
-        margin : "5% auto" ,
+        position : 'absolute',
+        top : '25%' ,
+        left : '28%' ,
     } ,
     container : {
-        maxWidth : '550px' ,
-        minHeight : '550px' ,
-        background : "linear-gradient(160deg, #6C6C6C 10%, #E0E0E0 80%)" ,
-        overflow : "visible"
+        width : '500px' ,
+        height : '500px' ,
+        backgroundColor : '#D0D0D0'
     } ,
     img : {
-        minWidth : '400px' ,
-        maxHeight : '500px' ,
-        margin : "auto" ,
-        display: "flex" ,
-        justifyContent : "center" ,
+        width : '300px' ,
+        height : '400px' ,
+        position : 'absolute',
+        top : '10%' ,
+        left : '25%' ,
+    } ,
+    awesomeicon : {
+        marginLeft : '10px' ,
+        fontSize : '25px' ,
+        color : 'fff'
     } ,
     button : {
-        margin : "2% auto" ,
-        display: "flex" ,
-        justifyContent : "center" ,
         background : 'linear-gradient(50deg, #00bfa5 40%, #00acc1 85%)' ,
-        color : "#fff"
-    } ,
-    open_paper : {
-        maxWidth : '500px' ,
-        maxHeight : '600px' ,
-        background : 'linear-gradient(160deg, #6C6C6C 10%, #E0E0E0 80%)' ,
-        margin : "auto" ,
-        alignItems : "center"
-    } ,
+        left : "40%"
+    }
+}
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    }
   }));
 
-export default function MenuAppBar() {
+  export default function Profilepage() {
     const classes = useStyles();
 
-    const [open, setOpen] = React.useState(false);
-
-    const handleOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const [member, setMember] = useState([]);
-    // const memberList = ['memberName', 'memberID', 'memberGender', 'memberBloodType', 'memberBirthday', 'memberEmail', 'memberAddress'];
-    useEffect(() => {
-        async function fetchDataMem() {
-                const result = await axios.get("/api/member/actforfun@gmail.com")
-                setMember(result.data);
-                console.log(result);           
-                // .then(result => {
-                //     setMember(result.data)
-                //     console.log(result)
-                // }).catch(err => {
-                //     console.log(err)
-                // })
-        }
-        fetchDataMem();
-    }, []);
-
-    const [organizer, setOrganizer] = useState([]);
-    // const organizerList = ['organizerName' , 'organizerEmail' , 'organizerPhone' ,'organizerAddress' , 'organizerInfo'];
-    useEffect(() => {
-        async function fetchDataOrg() {
-                const result = await axios.get("/api/organizer/actforfun@gmail.com");
-                setOrganizer(result.data);             
-                // .then(res => {
-                //     setMember(res.data)
-                //     console.log(res)
-                // }).catch(err => {
-                //     console.log(err)
-                // })
-        }
-        fetchDataOrg();
-    }, []);
-
-    return (
-        <div className={classes.div}>
-            <Header />
-            <div className={classes.left_menu}>
-                <Container className={classes.left_container}>
-                    <Typography variant="h5">
-                            <Box lineHeight="normal" m={4}>
-                                <Avatar className={classes.avatar} src="./img/profile.jpg" alt="user" />
-                            </Box>
-                            <Box lineHeight={2} m={1}>
-                                {member.memberName}
-                            </Box>
-                            <Divider />    
-                            <Link to="/profile" className={classes.link}>
-                                <Box lineHeight={1} m={4}>
-                                    個人檔案
-                                </Box>
-                            </Link>
-                            <Link to="/trainingFace" className={classes.link}>
-                                <Box lineHeight={1} m={4} color="#000">
-                                    訓練人臉
-                                </Box>
-                            </Link>
-                            <Link to="/signupSituation" className={classes.link}>
-                                <Box lineHeight={1} m={4}>
-                                    報名狀況
-                                </Box>
-                            </Link>
-                            <Divider />
-                            <Box lineHeight={3} m={1}>
-                                {organizer.organizerName}
-                            </Box>
-                            <Divider />
-                            <Link to="/organizerInfo" className={classes.link}>
-                                <Box lineHeight={1} m={4} >
-                                    主辦單位資訊
-                                </Box>
-                            </Link>    
-                            <Link to="/manageActivity" className={classes.link}>
-                                <Box lineHeight={1} m={4}>
-                                    管理活動
-                                </Box>
-                            </Link>
-                            <Divider />
-                            <Link to="/" className={classes.link}>
-                                <Box lineHeight={2} m={1}>
-                                    我的相簿
-                                </Box>
-                            </Link>
-                    </Typography>
-                </Container>
-                <Container className={classes.content}>
-                    <div>
-                        <Typography variant="h4">
-                            訓 練 人 臉
+    return(
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar style={style.toolbar}>
+                    <Button
+                        edge="start"
+                        style={style.back_button}
+                        color="inherit"
+                        display="none"
+                        // onClick={}
+                    >
+                        <FontAwesomeIcon icon={faArrowAltCircleLeft} style={style.awesomeicon} />
+                        <Typography variant="h6" style={style.title}>
+                            返 回 首 頁
                         </Typography>
-                        <hr />
-                    </div>
-                    <div>
-                        <form className={classes.form}>
-                                <Box lineHeight="normal" m={1}>
-                                    <Container className={classes.container}>
-                                        <img className={classes.img} src="./img/profile.jpg" alt="img" />
-                                        <Button
-                                            variant="contained"
-                                            className={classes.button}
-                                            onClick={handleOpen}
-                                            startIcon={<SyncAltIcon />}
-                                        >
-                                            開始訓練
-                                        </Button>
-                                    </Container>
-                                </Box>
-                                <Box lineHeight={1} m={1}>
-                                    
-                                    <Modal
-                                        open={open}
-                                        onClose={handleClose}
-                                        closeAfterTransition
-                                        BackdropComponent={Backdrop}
-                                        BackdropProps={{
-                                            timeout: 1000,
-                                        }}
-                                    >
-                                        <Fade in={open}>
-                                            <div className={classes.open_paper}>
-                                                <img className={classes.img} src="./img/profile.jpg" alt="img" />
-                                                <Button
-                                                    variant="contained"
-                                                    className={classes.button}
-                                                    startIcon={<BackupIcon />}
-                                                >
-                                                    完成訓練
-                                                </Button>
-                                            </div>
-                                        </Fade>
-                                    </Modal>
-                                </Box>
-                            </form>
-                        </div>  
-                </Container>
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            <div>
+                <Typography variant="h5" style={style.text_leftside}>
+                    <Box lineHeight="normal" m={1}>
+                        王小明
+                    </Box>
+                    <Box lineHeight={1} m={4}>
+                        個人檔案
+                    </Box>
+                    <Box lineHeight={1} m={4} style={{color : "#000000"}}>
+                        訓練人臉
+                    </Box>
+                    <Box lineHeight={1} m={4}>
+                        報名狀況
+                    </Box>
+                    <Box lineHeight={3} m={1}>
+                        主辦單位名稱
+                    </Box>
+                    <Box lineHeight={1} m={4}>
+                        主辦單位資訊
+                    </Box>
+                    <Box lineHeight={1} m={4}>
+                        管理活動
+                    </Box>
+                    <Box lineHeight={3} m={1}>
+                        我的相簿
+                    </Box>
+                </Typography>
+                <div style={style.line1}>
+                    <hr width="1" size="145" color="#6C6C6C" />
+                </div>
+                <div style={style.line2}>
+                    <hr width="1" size="85" color="#6C6C6C" />
+                </div>
+                <div style={style.line3}>
+                    <hr width="1" size="650" color="#E0E0E0" />
+                </div>
+            </div>
+            <div>
+                <Typography variant="h4" style={style.text_title}>
+                    訓 練 人 臉
+                    <hr width="800" />
+                </Typography>
+                <form style={style.form}>
+                    <Box lineHeight="normal" m={1}>
+                        <Container>
+                            <Typography style={style.container}>
+                                <img src={profile} style={style.img} alt="img" />
+                            </Typography>
+                        </Container>
+
+                    </Box>
+                    <Box lineHeight={5} m={1}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            style={style.button}
+                            startIcon={<SyncAltIcon />}
+                        >
+                        開始訓練
+                        </Button>
+                    </Box>
+                </form>
+
+
             </div>
         </div>
     );
-}
+  }
