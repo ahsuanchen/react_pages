a48ead596edb88dc2971a96611f16c798bdce774
ahsuanchen <ahsuan1027@gmail.com>
2020-03-26 21:07:14 +0800

    537

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import News from 'pages/News/index';
 import routess from 'routes';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
